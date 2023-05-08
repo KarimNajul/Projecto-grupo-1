a516e3874765be8cb9f661cab2f71568d0a79e66
Emiliano <liottae90@gmail.com>
2023-05-08 15:01:44 -0300

    Cambios en Tailwind.config, Instalacion feather-icons

--- ProyectoFrontEnd/src/App.jsx
+++ ProyectoFrontEnd/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+// import Carousel from "./components/Carousel"
+
+// const slides = [
+//   "https://i.ibb.co/ncrXc2V/1.png", 
+//   "https://i.ibb.co/B3s7v4h/2.png",
+//   "https://i.ibb.co/XXR8kzF/3.png",
+//   "https://i.ibb.co/yg7BSdM/4.png",
+// ];
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <h1 className='text-3xl font-bold underline'>Hola mundo!</h1>
+        <h1 className='font-bold underline text-red-700'>Hola mundo!</h1>
       </div>
+      {/* <Carousel>
+        {slides.map ((s) => (
+          <img src={s} alt="Images" />
+        ))}
+      </Carousel> */}
     </>
   )
 }
