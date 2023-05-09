13e5fe3e15778b05e9e98475a00d3c99a04c7240
Emiliano <liottae90@gmail.com>
2023-05-09 18:03:10 -0300

    Prueba de carousel

--- ProyectoFrontEnd/src/App.jsx
+++ ProyectoFrontEnd/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-// import Carousel from "./components/Carousel"
+import Carousel from "./components/Carousel"
 
-// const slides = [
-//   "https://i.ibb.co/ncrXc2V/1.png", 
-//   "https://i.ibb.co/B3s7v4h/2.png",
-//   "https://i.ibb.co/XXR8kzF/3.png",
-//   "https://i.ibb.co/yg7BSdM/4.png",
-// ];
+const slides = [
+  "https://i.ibb.co/ncrXc2V/1.png", 
+  "https://i.ibb.co/B3s7v4h/2.png",
+  "https://i.ibb.co/XXR8kzF/3.png",
+  "https://i.ibb.co/yg7BSdM/4.png",
+];
 
 function App() {
   
@@ -16,11 +16,13 @@ function App() {
       <div>
         <h1 className='font-bold underline text-red-700'>Hola mundo!</h1>
       </div>
-      {/* <Carousel>
+      <div className= "max-w-lg">
+      <Carousel>
         {slides.map ((s) => (
           <img src={s} alt="Images" />
         ))}
-      </Carousel> */}
+      </Carousel>
+      </div>
     </>
   )
 }
