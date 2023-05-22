82d35f85b792de982246c584ed0a7cc9e36e9e1e
tomaspanza <tomasjosuep@hotmail.com>
2023-05-22 16:20:09 -0300

    Descripcion de los productos en las cards

--- ProyectoFrontEnd/src/components/Product.jsx
+++ ProyectoFrontEnd/src/components/Product.jsx
@@ -1,24 +1,55 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
 
 const Product = ({ product }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className='flex flex-col border border-black bg-green-500'>
       <div>
         <div id='image' className='w-full h-64 object-cover'>
-          <img  src={product.image} alt={product.name} />
+          <img src={product.image} alt={product.name} />
         </div>
       </div>
       <div>
         <h1 className='text-red-600 p-4 font-bold'>{product.name}</h1>
-        <h2 className='text-red-500 p-4 underline'>{product.description}</h2>
       </div>
       <div className='text-center text-blue-400'>
         <h3>${product.price}</h3>
       </div>
       <div className='text-white text-center drop-shadow-2xl	 
-      border border-white m-3.5 bg-yellow-500 hover:bg-black active:bg-green-800'>
-        <button>Compra gil</button>
+      border border-solid border-2 border-white rounded-md m-3.5 bg-yellow-500 hover:bg-black active:bg-green-800'>
+        <button onClick={openModal}>Ver descripción</button>
       </div>
+      <div className='text-white text-center drop-shadow-2xl	 
+      border border-solid border-2 border-white rounded-md m-3.5 bg-yellow-500 hover:bg-black active:bg-green-800'>
+        <button >Agregar al carrito</button>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Descripción del producto"
+        className={'text-red-500 bg-black container mx-auto'}
+      >
+        <div className=''>
+          <h1>{product.name}</h1>
+          <div className='w-64 h-64 object-cover'>
+          <img src={product.image} alt={product.name} />
+          </div>
+          <h2>Descripción</h2>
+          <p>{product.description}</p>
+          <button onClick={closeModal}>Cerrar</button>
+          </div>
+      </Modal>
     </div>
   );
 };
@@ -33,3 +64,5 @@ Product.propTypes = {
 };
 
 export default Product;
+
+
