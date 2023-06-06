9f093a1ef725f01cb5cfbe0182cf5287a93b4700
Mati <matias.portillo996@gmail.com>
2023-06-06 14:04:56 -0300

    Correciones

--- ProyectoFrontEnd/src/pages/Inicio.jsx
+++ ProyectoFrontEnd/src/pages/Inicio.jsx
@@ -1,9 +1,11 @@
 import Carousel from "../components/Carousel";
+import Instagram from "../components/CarouselInsta";
 
 const Inicio = () => {
   return (
     <div className="relative z-0">
       <Carousel></Carousel>
+      <Instagram></Instagram>
     </div>
   );
 };
